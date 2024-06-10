4983cf5858a265225015b4341f5e6992897bde4d
Matheus Gonçalves Rodrigues <mgrbq@outlook.com>
2024-06-10 17:26:32 -0300

    Created model and consumer for user and added persistence project

---
+++ Dummy.QueueProcessor/Consumers/UserQueue.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using System.Text.Json;
+
+public class UserQueue : IConsumer<UserModel>
+{
+    public Task Consume(ConsumeContext<UserModel> context)
+    {
+        var user = context.Message;
+
+        Console.WriteLine(JsonSerializer.Serialize(user));
+
+        return Task.CompletedTask;
+    }
+}
--- Dummy.QueueProcessor/Program.cs
+++ Dummy.QueueProcessor/Program.cs
@@ -1,4 +1,24 @@
+using DummyConsumer.Consumers;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<UserQueue>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", 1572, "/", h => {
+            h.Username("admin");
+            h.Password("admin");
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
