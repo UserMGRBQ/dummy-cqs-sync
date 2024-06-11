f565686136662a67e97b36bf082bb8c0a0153aa3
Matheus Gonçalves Rodrigues <mgrbq@outlook.com>
2024-06-11 17:22:52 -0300

    Created implementation for ICommandUserRepository and IQueryUserRepository

---
+++ Dummy.Persistence/Repositories/Queries/QueryUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Dummy.Core.Interfaces.Repositories.Queries;
+using Dummy.Core.Models;
+using Dummy.Persistence.Contexts;
+using Dummy.Persistence.Repositories.Base;
+
+namespace Dummy.Persistence.Repositories.Queries;
+
+public class QueryUserRepository(DummyQueryContext context) : BaseQueryRepository<UserModel, int>(context), IQueryUserRepository
+{
+}
